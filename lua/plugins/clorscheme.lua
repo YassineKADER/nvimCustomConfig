b81371a8f3c80e1992023762e8460b8d06b3e776
EROS <yassinekader.contact@gmail.com>
2024-04-18 20:20:12 +0100

    init

---
+++ lua/plugins/clorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github_dark_high_contrast",
+    },
+  },
+}
